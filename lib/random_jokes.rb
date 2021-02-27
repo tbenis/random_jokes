609f8461735c83bc50f18d522e780765c342acb1
Benis Tambe <will.jenifer@yahoo.com>
2021-02-26 20:40:39 -0500

    initial commit

---
+++ lib/random_jokes.rb
@@ -0,0 +1,6 @@
+require "random_jokes/version"
+
+module RandomJokes
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/random_jokes/version.rb
@@ -0,0 +1,3 @@
+module RandomJokes
+  VERSION = "0.1.0"
+end
