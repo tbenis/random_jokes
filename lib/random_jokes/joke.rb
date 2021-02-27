6faf24e57d6745613fb219212632de210cac1cfb
Benis Tambe <will.jenifer@yahoo.com>
2021-02-27 15:29:55 -0500

    Cleaning up

--- lib/random_jokes/joke.rb
+++ lib/random_jokes/joke.rb
@@ -1,18 +1,9 @@
 class RandomJokes::Joke
 
-=begin
-    get programming_jokes
-    get general_joke
-    print single joke
-    
-    a joke should have an ID, type, setup (Wait 3 secs), punchline
-
-=end
     def initialize(joke)
         @get_joke_type = joke["type"]
         @joke_setup = joke["setup"]
         @joke_punchline = joke["punchline"]
-        # puts "Hello #{joke}".blue
         print_joke
     end
 
